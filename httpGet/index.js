c37de29fa4dcfa930c6476bf7af1a805da8774e4
Ulrik Strid <ulrik.strid@outlook.com>
2017-08-11 13:07:49 +0200

    Move stuff inside function

--- httpGet/index.js
+++ httpGet/index.js
@@ -2,35 +2,35 @@ import graphQLServer from "@ulrikstrid/apollo-server-azure-functions";
 import graphqlTools from "graphql-tools";
 import { makeExecutableSchema } from "graphql-tools";
 
-console.log(graphqlTools);
-
-const typeDefs = `
-  type Random {
-    id: Int!
-    rand: String
-  }
-
-  type Query {
-    rands: [Random]
-    rand(id: Int!): Random
-  }
-`;
-
-const rands = [{ id: 1, rand: "random" }, { id: 2, rand: "modnar" }];
-
-const resolvers = {
-  Query: {
-    rands: () => rands,
-    rand: (_, { id }) => rands.find(rand => rand.id === id)
-  }
-};
-
-const schema = graphqlTools.makeExecutableSchema({
-  typeDefs,
-  resolvers
-});
-
 export default function run(context, request) {
+  console.log(graphqlTools);
+
+  const typeDefs = `
+    type Random {
+      id: Int!
+      rand: String
+    }
+
+    type Query {
+      rands: [Random]
+      rand(id: Int!): Random
+    }
+  `;
+
+  const rands = [{ id: 1, rand: "random" }, { id: 2, rand: "modnar" }];
+
+  const resolvers = {
+    Query: {
+      rands: () => rands,
+      rand: (_, { id }) => rands.find(rand => rand.id === id)
+    }
+  };
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+  });
+
   if (request.method === "POST") {
     server.graphqlAzureFunctions(context, request);
   } else if (request.method === "GET") {

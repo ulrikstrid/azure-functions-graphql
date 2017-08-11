12ecc512a217a1059a6676d816bf2c165139083c
Ulrik Strid <ulrik.strid@outlook.com>
2017-08-11 12:53:48 +0200

    change to graphql

--- httpGet/index.js
+++ httpGet/index.js
@@ -1,8 +1,36 @@
-export default function run(context, request) {
-  context.res = {
-    body: JSON.stringify({ status: "OK" }),
-    status: 200
-  };
+import graphQLServer from "@ulrikstrid/apollo-server-azure-functions";
+import graphqlTools from "graphql-tools";
 
-  context.done(null);
+const typeDefs = `
+  type Random {
+    id: Int!
+    rand: String
+  }
+
+  type Query {
+    rands: [Random]
+    rand(id: Int!): Random
+  }
+`;
+
+const rands = [{ id: 1, rand: "random" }, { id: 2, rand: "modnar" }];
+
+const resolvers = {
+  Query: {
+    rands: () => rands,
+    rand: (_, { id }) => rands.find(rand => rand.id === id)
+  }
+};
+
+const schema = graphqlTools.makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+export default function run(context, request) {
+  if (request.method === "POST") {
+    server.graphqlAzureFunctions(context, request);
+  } else if (request.method === "GET") {
+    return server.graphiqlAzureFunctions(context, request);
+  }
 }
